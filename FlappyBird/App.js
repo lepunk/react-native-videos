9dd76ec67f9e829fec2a7e9f18b14d07fd8c98c6
Tamas Szikszai <submitterbot@gmail.com>
2019-09-11 17:02:38 +0100

    spawn pipes

--- FlappyBird/App.js
+++ FlappyBird/App.js
@@ -9,24 +9,6 @@ import Physics from './Physics';
 import Constants from './Constants';
 import Images from './assets/Images';
 
-export const randomBetween = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-export const generatePipes = () => {
-    let topPipeHeight = randomBetween(100, (Constants.MAX_HEIGHT / 2) - 100);
-    let bottomPipeHeight = Constants.MAX_HEIGHT - topPipeHeight - Constants.GAP_SIZE - 50;
-
-    let sizes = [topPipeHeight, bottomPipeHeight]
-
-    if (Math.random() < 0.5) {
-        sizes = sizes.reverse();
-    }
-
-
-    return sizes;
-}
-
 export default class App extends Component {
     constructor(props){
         super(props);
--- FlappyBird/Physics.js
+++ FlappyBird/Physics.js
@@ -1,7 +1,27 @@
 import Matter from "matter-js";
+import Constants from './Constants';
+import Pipe from './Pipe';
 
 let tick = 0;
 let pose = 1;
+
+export const randomBetween = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+export const generatePipes = () => {
+    let topPipeHeight = randomBetween(100, (Constants.MAX_HEIGHT / 2) - 100);
+    let bottomPipeHeight = Constants.MAX_HEIGHT - topPipeHeight - Constants.GAP_SIZE - 50;
+
+    let sizes = [topPipeHeight, bottomPipeHeight]
+
+    if (Math.random() < 0.5) {
+        sizes = sizes.reverse();
+    }
+
+
+    return sizes;
+}
 
 const Physics = (entities, { touches, time }) => {
     let engine = entities.physics.engine;
@@ -14,6 +34,21 @@ const Physics = (entities, { touches, time }) => {
             if (world.gravity.y === 0.0){
                 // first press really
                 world.gravity.y = 1.2;
+                
+                let [pipe1Height, pipe2Height] = generatePipes();
+
+                let pipe1 = Matter.Bodies.rectangle( Constants.MAX_WIDTH - (Constants.PIPE_WIDTH / 2), pipe1Height / 2, Constants.PIPE_WIDTH, pipe1Height, { isStatic: true });
+                let pipe2 = Matter.Bodies.rectangle( Constants.MAX_WIDTH - (Constants.PIPE_WIDTH / 2), Constants.MAX_HEIGHT - (pipe2Height / 2) - 50, Constants.PIPE_WIDTH, pipe2Height, { isStatic: true });
+
+                Matter.World.add(world, [pipe1, pipe2]);
+
+                entities["pipe1"] = {
+                    body: pipe1, size: [Constants.PIPE_WIDTH, pipe1Height], color: "green", renderer: Pipe
+                }
+
+                entities["pipe2"] = {
+                    body: pipe2, size: [Constants.PIPE_WIDTH, pipe2Height], color: "green", renderer: Pipe
+                }
             }
             hadTouches = true;
             //Matter.Body.applyForce( bird, bird.position, {x: 0.00, y: -0.05});
