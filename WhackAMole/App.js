e8f81fb652bea8b1554aa94fd10435a6b983b032
Tamas Szikszai <submitterbot@gmail.com>
2019-09-18 07:35:00 +0100

    show those moles

--- WhackAMole/App.js
+++ WhackAMole/App.js
@@ -8,8 +8,31 @@ import Mole from './Mole';
 export default class App extends Component {
     constructor(props){
         super(props);
+        this.moles = [];
+        this.molesPopping = 0;
 
-        this.mole = null;
+        this.interval = setInterval(this.popRandomMole, 350);
+    }
+
+    randomBetween = (min, max) => {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    onFinishPopping = (index) => {
+        this.molesPopping -= 1;
+    }
+
+    popRandomMole = () => {
+        if (this.moles.length != 12){
+            console.log(this.moles.length);
+            return;
+        }
+
+        let randomIndex = this.randomBetween(0, 11);
+        if (!this.moles[randomIndex].isPopping && this.molesPopping < 3){
+            this.molesPopping += 1;
+            this.moles[randomIndex].pop();
+        }
     }
 
     render(){
@@ -17,13 +40,15 @@ export default class App extends Component {
             <View style={styles.container}>
                 <Image style={styles.backgroundImage} resizeMode="stretch" source={Images.background} />
                 <View style={styles.playArea}>
-                    {Array.apply(null, Array(4)).map((el, idx) => {
+                    {Array.apply(null, Array(4)).map((el, rowIdx) => {
                         return (
-                            <View style={styles.playRow} key={idx}>
-                                {Array.apply(null, Array(3)).map((el, idx) => {
+                            <View style={styles.playRow} key={rowIdx}>
+                                {Array.apply(null, Array(3)).map((el, colIdx) => {
+                                    let moleIdx = (rowIdx * 3) + colIdx;
+
                                     return (
-                                        <View style={styles.playCell} key={idx}>
-                                            <Mole />
+                                        <View style={styles.playCell} key={colIdx}>
+                                            <Mole index={moleIdx} ref={(ref) => { this.moles[moleIdx] = ref }} onFinishPopping={this.onFinishPopping} />
                                         </View>
                                     )
                                 })}
--- WhackAMole/Mole.js
+++ WhackAMole/Mole.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Button, Image } from 'react-native';
+import { View, StyleSheet, Button, Image, TouchableWithoutFeedback } from 'react-native';
 import Images from './assets/Images';
 import SpriteSheet from 'rn-sprite-sheet';
 import Constants from './Constants';
@@ -9,25 +9,79 @@ export default class Mole extends Component {
         super(props);
 
         this.mole = null;
+        this.disappearTimeout = null;
+        this.isPopping = false;
+    }
+
+    pop = () => {
+        this.isPopping = true;
+        this.mole.play({
+            type: "appear",
+            fps: 24,
+            onFinish: () => {
+                this.disappearTimeout = setTimeout(() => {
+                    this.mole.play({
+                        type: "hide",
+                        fps: 24,
+                        onFinish: () => {
+                            this.isPopping = false;
+                            this.props.onFinishPopping(this.props.index);
+                        }
+                    })
+                }, 1000)
+            }
+        })
+    }
+
+    whack = () => {
+        if (this.disappearTimeout){
+            clearTimeout(this.disappearTimeout);
+        }
+
+        if (!this.isPopping){
+            return;
+        }
+
+        this.mole.play({
+            type: "dizzy",
+            fps: 24,
+            onFinish: () => {
+                this.mole.play({
+                    type: "faint",
+                    fps: 24,
+                    onFinish: () => {
+                        this.isPopping = false;
+                        this.props.onFinishPopping(this.props.index);
+                    }
+                })
+            }
+        })
     }
 
     render(){
         return (
-            <SpriteSheet
-                ref={ref => (this.mole = ref)}
-                source={Images.sprites}
-                columns={6}
-                rows={8}
-                // height={200} // set either, none, but not both
-                // width={200}
-                //width={100}
-                width={100}
-                animations={{
-                    idle: [0],
-                    appear: [1, 2, 3, 4],
-                    hide: [4, 3, 2, 1, 0],
-                }}
-            />
+            <View style={{ flex: 1 }}>
+                <SpriteSheet
+                    ref={ref => (this.mole = ref)}
+                    source={Images.sprites}
+                    columns={6}
+                    rows={8}
+                    // height={200} // set either, none, but not both
+                    // width={200}
+                    //width={100}
+                    width={100}
+                    animations={{
+                        idle: [0],
+                        appear: [1, 2, 3, 4],
+                        hide: [4, 3, 2, 1, 0],
+                        dizzy: [36, 37, 38],
+                        faint: [42, 43, 44, 0]
+                    }}
+                />
+                <TouchableWithoutFeedback onPress={this.whack} style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0}}>
+                    <View style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }} />
+                </TouchableWithoutFeedback>
+            </View>
         )
     }
 }
